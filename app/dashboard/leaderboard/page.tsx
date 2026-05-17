0fcc6b790069deed82129aad47304820417475fe
adamuchindoo <49189545+adamuchindoo@users.noreply.github.com>
2026-05-18 05:30:39 +0800

    new update

--- app/dashboard/leaderboard/page.tsx
+++ app/dashboard/leaderboard/page.tsx
@@ -78,7 +78,7 @@ export default function LeaderboardPage() {
               <tr>
                 <th className="text-left p-4">Position</th>
 
-                <th className="text-left p-4">Full Name</th>
+                <th className="text-left p-4">Name</th>
 
                 <th className="text-left p-4">Points</th>
               </tr>
